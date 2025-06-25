a29cb92cdd5cd7118b7d66013248c760bd0f1371
Shiva Kumar Souta <shivakumarsouta18@gmail.com>
2025-06-25 18:52:04 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Shiva Kumar Souta',
+    author_email= 'shivakumarsouta18@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
